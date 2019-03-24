631b54781d8a409b6e7733286c8609718c949a76
Spencer Carlson <spencerdcarlson@gmail.com>
2019-03-24 16:52:58 -0600

    update readme

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JQ.MixProject do
   def project do
     [
       app: :jq,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.7",
       description: "elixir wrapper for jq",
       package: package(),
