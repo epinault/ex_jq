52ccb2b6854522d2c9485fce4a85fefd4c885587
Spencer Carlson <spencerdcarlson@gmail.com>
2019-07-02 21:32:32 -1000

    testing

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :logger,
 config :husky,
   pre_commit: "mix format",
   pre_push: "mix test"
+
 #  host_path: "../jq",
 #  escript_path: "/Users/sc/code/husky-elixir/priv/husky"
 #  json_codec: Jason
